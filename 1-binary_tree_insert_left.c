a0c96de8b01371e10547c0cb133169c3b5f3c733
Miniknacky <jonathanhuybrechts@gmail.com>
2024-01-09 12:25:35 +0000

    add stdlib

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,14 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Inserts a node as the left-child of another node
- * @parent: Pointer to the node to insert the left-child in
- * @value: Value to store in the new node
+ * binary_tree_insert_left - Inserts a node as the left-child of another node.
+ * @parent: Pointer to the node to insert the left-child in.
+ * @value: Value to store in the new node.
  *
- * Return: Pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: Pointer to the created node, or NULL on failure or if parent
+ * is NULL.
  *
  * Description: If parent already has a left-child, the new node must take its
- * place, and the old left-child must be set as the left-child of the new node.
+ * place, and the old left-child must be set as the left-child
+ * of the new node.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -24,8 +26,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
+	new_node->right = NULL;
+
 	old_left = parent->left;
-	new_node->right = old_left;
+	new_node->left = old_left;
 
 	if (old_left != NULL)
 		old_left->parent = new_node;
